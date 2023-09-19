a17d648896f0186cdb5529422efe45ac83e874d3
Cesar Santos <cesar.santos2042@gmail.com>
2023-09-19 00:03:52 -0300

    starting recipe model

---
+++ src/main/java/com/recipes/app/model/Recipe.java
@@ -0,0 +1,10 @@
+package com.recipes.app.model;
+
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private String description;
+    private List<String> ingredientList;
+
+}
--- src/main/java/com/recipes/app/model/User.java
+++ src/main/java/com/recipes/app/model/User.java
@@ -14,6 +14,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String phone;
 
     public Long getId() {
         return id;
@@ -29,6 +30,14 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 
 }
